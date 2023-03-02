df1fda277afcaa016ea21ddbb728ad54118b7a1a
IVenzon <ijv2543@gmail.com>
2023-03-02 13:10:41 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-# CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+# CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar' # uncomment this line and comment the line below if on Mac
 CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
